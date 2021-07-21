c176627e0cea59d83235f71cf623b80f012aecdf
shahrizodasub <shahrizodasubhonova2006@gmail.com>
2021-07-21 17:57:01 +0500

    first commit

---
+++ posts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+
+    text = models.TextField()
+    
+
+    def __str__(self): 
+        return self.text[:50]
+
+
+
